d4e76a45653e578a750c58cfca80c71d6290e58e
Isaac Way <iway1@vols.utk.edu>
2021-04-22 23:44:55 -0400

    Fixed indentation

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
-import {FourCorners} from './FourCorners';
+import { FourCorners } from './FourCorners';
 
 const items = [
-  {imageSource: require('./assets/beach.jpg'), text: 'Beach'},
-  {imageSource: require('./assets/forest.jpg'), text: 'Forest'},
-  {imageSource: require('./assets/desert.jpg'), text: 'Desert'},
-  {imageSource: require('./assets/plains.jpg'), text: 'Plains'},
+  { imageSource: require('./assets/beach.jpg'), text: 'Beach' },
+  { imageSource: require('./assets/forest.jpg'), text: 'Forest' },
+  { imageSource: require('./assets/desert.jpg'), text: 'Desert' },
+  { imageSource: require('./assets/plains.jpg'), text: 'Plains' },
 ]
 
 export default function App() {
   const [selectedIndex, setSelectedIndex] = useState(null);
-  
+
   return (
     <View style={{
       flex: 1,
@@ -19,10 +19,10 @@ export default function App() {
       alignItems: 'center',
       padding: 15,
     }}>
-      <FourCorners 
+      <FourCorners
         items={items}
         selectedIndex={selectedIndex}
-        size={Dimensions.get('window').width-30}
+        size={Dimensions.get('window').width - 30}
       />
       <TouchableOpacity style={{
         justifyContent: 'center',
@@ -33,14 +33,14 @@ export default function App() {
         height: 100,
         marginTop: 20,
       }}
-        onPress={()=>{
-          if(selectedIndex == null)
+        onPress={() => {
+          if (selectedIndex == null)
             setSelectedIndex(0);
           else
             setSelectedIndex((selectedIndex + 1) % 4);
         }}
       >
-        <Text style={{color: 'white', fontSize: 36}}>
+        <Text style={{ color: 'white', fontSize: 36 }}>
           Next
         </Text>
       </TouchableOpacity>
